dfc05692a79844ffa406c5876b7336f5199ad47d
sourav <mishrasourav223@gmail.com>
2025-06-11 09:35:48 +0530

    Initial Projet
    Initial project setup.
    -Considering there will be no SwiftData or CoreData for the project.

---
+++ Pro Ads/Pro_AdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_AdsApp.swift
+//  Pro Ads
+//
+//  Created by Sourav on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Pro_AdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pro AdsTests/Pro_AdsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Pro_AdsTests.swift
+//  Pro AdsTests
+//
+//  Created by Sourav on 11/06/25.
+//
+
+import Testing
+@testable import Pro_Ads
+
+struct Pro_AdsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
